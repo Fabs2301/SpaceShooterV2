b3a9909a26f60dd99db8900e39828c3b27b9712a
Fabs <Fabs@Fabians-PC>
2019-10-20 20:16:09 +0200

    Test

--- src/Starter.java
+++ src/Starter.java
@@ -792,10 +792,10 @@ public class Starter extends Application implements EventHandler<KeyEvent> {
 			}
 			break;
 		case "Home":
-			if (setScene == 2) {
+			/**if (setScene == 2) {
 				setScene = 6;
 				moveText = 50;
-			}
+			}**/
 		}
 	}
 
